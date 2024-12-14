c4f42fd0390f042261f81f6d06d8771543b4ae8e
Alexander Mulyar <mulyar.alexander0503@yandex.ru>
2024-12-14 15:50:20 +0500

    Add styles

--- index.php
+++ index.php
@@ -12,10 +12,11 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" href="img/favicon.png">
+        <link rel="stylesheet" href="css/main.css">
     </head>
 
     <body>
-        <div class="clock">
+        <div class="clock border">
             <div class="time">
                 <span id="hours"><?php print($data["hours"]); ?></span>
                 <span class="blink-colon">:</span>
